96cc177f0e46a48b5bf2f3211b18c3cc56a3044e
Kshrugal R Jangalapalli <89105752+kshrugalj@users.noreply.github.com>
2024-04-13 05:07:13 -0500

    Add files via upload

---
+++ ScavengerHunt.session.sql
@@ -0,0 +1,20 @@
+CREATE Table AreasOfInterest(
+    id BIGINT,
+    top_latitude FLOAT,
+    bottom_latitude FLOAT,
+    left_longitude FLOAT,
+    right_longitude FLOAT
+
+);
+
+CREATE Table KeyLocations(CREATE Table KeyLocations(
+    id BIGINT,
+    location_id BIGINT,
+    latitude BIGINT,
+    longitude BIGINT
+);
+    id BIGINT,
+    location_id BIGINT,
+    latitude BIGINT,
+    longitude BIGINT
+);
