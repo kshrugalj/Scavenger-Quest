979367987c96161c707021917a1a841c80553ad2
Errorcode101 <78766995+Errorcode101@users.noreply.github.com>
2024-04-12 22:37:18 -0500

    Skeleton flutter framework
    A basic Skeleton to use for our app.

---
+++ scavenger_quest_frontend/README.md
@@ -0,0 +1,3 @@
+# scavenger_quest_frontend
+
+A new Flutter project.
---
+++ scavenger_quest_frontend/android/app/src/main/kotlin/com/example/scavenger_quest_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.scavenger_quest_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
